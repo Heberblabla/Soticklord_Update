75b93c0df3e86d551e16278f1d925a6d5928391a
Heberblabla <132624731+Heberblabla@users.noreply.github.com>
2025-11-10 22:23:35 -0500

    Update Rey_Borrego.kt

--- app/src/main/java/Data/Personalizados/Rey_Borrego.kt
+++ app/src/main/java/Data/Personalizados/Rey_Borrego.kt
@@ -14,6 +14,7 @@ import android.widget.Toast
 import io.ktor.client.plugins.BodyProgress
 
 
+
 class Rey_Borrego (
     Nivel:Int = 1
 ):
@@ -35,6 +36,7 @@ class Rey_Borrego (
         precision = 100
     ), Serializable {
 
+        
     var Resurreccion : Boolean = true //troopas
     var Revivir : Boolean = true //rey
     override fun toString(): String {
@@ -52,6 +54,7 @@ class Rey_Borrego (
         """.trimIndent()
     }
 
+    
     private fun daño(): Int {
         val random = Random.Default
         val suerte = random.nextDouble()
